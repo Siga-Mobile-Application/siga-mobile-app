fcfc11de992b3e032d038ebb29671d83ba3dff0c
Carlos Eduardo Fiori dos Santos <carlos.fiori@hotmail.com>
2024-08-25 20:52:16 -0300

    feat: firt setup of the project

---
+++ app/_layout.tsx
@@ -0,0 +1,7 @@
+import { Slot } from "expo-router";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
+export default function Layout() {
+    return <GluestackUIProvider mode="light"><Slot /></GluestackUIProvider>;
+}
