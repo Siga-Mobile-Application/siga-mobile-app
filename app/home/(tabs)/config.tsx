c766a7f53d4523a15aabc91d8d048e87a4e3cbd3
Carlos Eduardo Fiori dos Santos <carlos.fiori@hotmail.com>
2025-03-09 14:31:37 -0300

    feat: redesign configuration screen and increment new verifications version

--- app/home/(tabs)/config.tsx
+++ app/home/(tabs)/config.tsx
@@ -4,9 +4,10 @@ import { View } from "react-native";
 import * as Application from 'expo-application';
 import StyledText from "@/components/styled-text";
 import { apiAppVersion } from "@/helper/axios";
-import { Link } from "@/components/ui/link";
+import { Link, LinkText } from "@/components/ui/link";
 import { useIsFocused } from "@react-navigation/native";
 import HelperContext from "@/contexts/assistant";
+import { VStack } from "@/components/ui/vstack";
 
 export default function Configuration() {
     const [load, setLoad] = useState(true);
@@ -18,6 +19,8 @@ export default function Configuration() {
     const APPCURRENTVERSION = Application.nativeApplicationVersion;
 
     async function fetchAppVersion() {
+        setLoad(true);
+
         await apiAppVersion.get("latest").then((data) => {
             const responseURL: string = data.request.responseURL;
 
@@ -25,7 +28,7 @@ export default function Configuration() {
                 const latestVersion = responseURL.substring(responseURL.lastIndexOf('v') + 1);
 
                 if (latestVersion == APPCURRENTVERSION) {
-                    setMsg('Aplicativo na versão mais atual. \nVersão: ' + APPCURRENTVERSION);
+                    setMsg('Aplicativo na versão mais atual');
                 } else {
                     setMsg('Uma versão mais atualizada foi encontrada');
                 }
@@ -34,14 +37,16 @@ export default function Configuration() {
             }
         }).catch((e) => {
             setMsg("Erro ao verificar a versão");
+        }).finally(() => {
+            setLoad(false);
         });
-
-        setLoad(false);
     }
 
     useEffect(() => {
-        fetchAppVersion();
-    }, [isFocused, reload]);
+        if (isFocused) {
+            fetchAppVersion();
+        }
+    }, [reload]);
 
     return (
         load ?
@@ -52,10 +57,23 @@ export default function Configuration() {
 
             :
 
-            <View className='w-full h-full justify-center items-center'>
-                <StyledText text={msg} />
-                {msg.includes("atualizada") && <Link href={`${apiAppVersion.getUri()}latest`}><StyledText text="Clique aqui para baixar" /></Link>}
-            </View>
+            <VStack className="h-full items-center justify-between">
+                <View style={{ height: '50%', justifyContent: 'flex-end' }}>
+                    <StyledText size="lg" text={msg} />
+                    {
+                        msg.includes("atualizada") &&
 
+                        <Link href={`${apiAppVersion.getUri()}latest`}>
+                            <LinkText>
+                                Clique aqui para baixar
+                            </LinkText>
+                        </Link>
+                    }
+                </View>
+
+                <View style={{}}>
+                    <StyledText fontWeight="bold" size="lg" text={`Versão atual: ${APPCURRENTVERSION}`} />
+                </View>
+            </VStack>
     )
 }
