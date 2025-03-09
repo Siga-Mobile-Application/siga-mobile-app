c744f00e76308806402601db134d7706b30dac73
Carlos Eduardo Fiori dos Santos <carlos.fiori@hotmail.com>
2025-03-09 03:07:34 -0300

    feat: add version verification and message correspoding to it

--- app/home/(tabs)/config.tsx
+++ app/home/(tabs)/config.tsx
@@ -1,25 +1,47 @@
 import Loading from "@/components/loading";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { View } from "react-native";
 import * as Application from 'expo-application';
 import StyledText from "@/components/styled-text";
+import { apiAppVersion } from "@/helper/axios";
+import { Link } from "@/components/ui/link";
+import { useIsFocused } from "@react-navigation/native";
+import HelperContext from "@/contexts/assistant";
 
 export default function Configuration() {
     const [load, setLoad] = useState(true);
     const [msg, setMsg] = useState('');
+
+    const { reload } = useContext(HelperContext);
+    const isFocused = useIsFocused();
+
     const APPCURRENTVERSION = Application.nativeApplicationVersion;
 
-    function fetchAppVersion() {       
-        // if('2.4.32.19' === APPCURRENTVERSION) {
-        setMsg('Aplicativo na versão mais atual. \nVersão: ' + APPCURRENTVERSION);
-        // } else {
-        //     setMsg('Uma versão mais atualizada foi encontrada');
-        // }
+    async function fetchAppVersion() {
+        await apiAppVersion.get("latest").then((data) => {
+            const responseURL: string = data.request.responseURL;
+
+            if (!responseURL.endsWith("releases")) {
+                const latestVersion = responseURL.substring(responseURL.lastIndexOf('v') + 1);
+
+                if (latestVersion == APPCURRENTVERSION) {
+                    setMsg('Aplicativo na versão mais atual. \nVersão: ' + APPCURRENTVERSION);
+                } else {
+                    setMsg('Uma versão mais atualizada foi encontrada');
+                }
+            } else {
+                setMsg('Nenhuma versão encontrada');
+            }
+        }).catch((e) => {
+            setMsg("Erro ao verificar a versão");
+        });
 
         setLoad(false);
-    }    
+    }
 
-    useEffect(fetchAppVersion, []);
+    useEffect(() => {
+        fetchAppVersion();
+    }, [isFocused, reload]);
 
     return (
         load ?
@@ -32,6 +54,7 @@ export default function Configuration() {
 
             <View className='w-full h-full justify-center items-center'>
                 <StyledText text={msg} />
+                {msg.includes("atualizada") && <Link href={`${apiAppVersion.getUri()}latest`}><StyledText text="Clique aqui para baixar" /></Link>}
             </View>
 
     )
--- helper/axios.ts
+++ helper/axios.ts
@@ -2,12 +2,17 @@ import axios from "axios";
 import Constants from "expo-constants";
 
 const uri = process.env.EXPO_PUBLIC_GET_DATA_API ? process.env.EXPO_PUBLIC_GET_DATA_API : `http://${Constants.expoConfig?.hostUri?.split(':').shift()}:3000`;
+const uriAppVersion = "https://github.com/Siga-Mobile-Application/siga-mobile-app/releases/";
 
 const api = axios.create({
    baseURL: uri,
    timeout: 30000
 });
 
-// api.defaults.withCredentials = true;
+const apiAppVersion = axios.create({
+   baseURL: uriAppVersion,
+   timeout: 4000
+});
 
+export { apiAppVersion };
 export default api;
